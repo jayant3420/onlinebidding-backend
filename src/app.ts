69c725dfb24700af72c5daea927fec6d0d355757
jayant kumar <jayant.3420@gmail.com>
2024-11-03 18:57:33 +0530

    update

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Request, Response } from "express";
@@ -6,8 +6,19 @@ import publicRoutes from "./route/public";
 import product from "./route/product/product.routes";
 import bid from "./route/bid/bid.routes";
 import { errorHandler } from "./middleware/error.handler";
+import cors, { CorsOptions } from "cors";
 
 const app = express();
+
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:3600",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept", "x-api-key"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(
   express.urlencoded({ parameterLimit: 100000, limit: "25mb", extended: true })
 );
@@ -22,7 +33,6 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/v1", publicRoutes);
 app.use("/api/v1/product", product);
 app.use("/api/v1/bid", bid);
-
 
 app.use(errorHandler);
 
